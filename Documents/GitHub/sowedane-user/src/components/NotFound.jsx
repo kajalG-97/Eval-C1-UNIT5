ca1e04052c606edbcc5655e39924e9149761624f
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-05-09 21:08:09 +0530

    .

--- Documents/GitHub/sowedane-user/src/components/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-export const NotFound = () => {
-  return (
-    <div className="notFound">
-      <img src="	https://miro.medium.com/max/1400/1*zBFBJktPD3_z0S_35kO5Hg.gif" alt="pic" />
-      </div>
-  );
-};
--- Documents/GitHub/sowedane-user/src/redux/status/statusAction.js
+++
@@ -1,7 +0,0 @@
-export const ADMIN_STATUS = "ADMIN_STATUS";
-
-export const USER_STATUS = "USER_STATUS";
-
-export const adminStatus = () => ({ type: ADMIN_STATUS });
-
-export const userStatus = () => ({ type: USER_STATUS });
